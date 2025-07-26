2f7cebe48a52616506e8ff9823bf68fe5effa1ab
2330Ola <jolao@miumg.edu.gt>
2025-07-25 20:03:52 -0600

    Se implemento la funcionalidad de actualización y eliminación de elementos en memory_db.py; agregar el modelo ItemUpdate en item.py.

--- app/models/item.py
+++ app/models/item.py
@@ -10,9 +10,6 @@ class ItemCreate(BaseModel):
     name: str
     price: float
 
-# TODO: EJERCICIO PARA ESTUDIANTES
-# Crear aquí el modelo ItemUpdate para las operaciones PUT
-# Pista: Debe tener los mismos campos que ItemCreate
-# class ItemUpdate(BaseModel):
-#     name: str
-#     price: float 
+class ItemUpdate(BaseModel):
+    name: str
+    price: float 
